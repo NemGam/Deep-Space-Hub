45baa69e53a4945c564d74720b75072bcc025389
vkrukhmalev <Krukhmalev02@gmail.com>
2024-11-17 22:10:52 -0500

    added filters

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,13 @@
 import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
-export default function Header(){
+export default function Header({onSearchChange}){
 
     return(
         <header className={styles.header}>
             <div className={styles.content}> 
-                <Link to="/"><h1>DeepSpaceHub 🚀</h1></Link>
+                <Link to="/" className={styles.logo}><h1>DeepSpaceHub 🚀</h1></Link>
+                <div className={styles.profile}></div>
             </div>
             
         </header>
--- src/pages/feed/Feed.jsx
+++ src/pages/feed/Feed.jsx
@@ -5,11 +5,39 @@ import { useEffect, useState } from "react";
 
 export default function Feed() {
     const [posts, setPosts] = useState([]);
+    const [search, setSearch] = useState("");
+    const [sort, setSort] = useState("time");
+
+    const sortPosts = (arr) => {
+        const temp = [...arr];
+
+        switch (sort)
+        {
+            case "gravity":
+                temp.sort((a, b) => b.gravity - a.gravity);
+                break;
+            case "time":
+                temp.sort((a, b) => b.postedAt - a.postedAt);
+                break;
+            case "name":
+                temp.sort((a, b) => a.title.localeCompare(b.title));
+                break;
+            default:
+                break;
+        }
+        return temp;
+    }
+
+    //Sort
+    useEffect(() => {
+        setPosts(sortPosts(posts));
+
+    }, [sort])
 
 
     useEffect(() => {
 
-        const getPosts = async() => {
+        const getPosts = async () => {
             //Fetch posts
             const res = [
                 {
@@ -22,48 +50,75 @@ export default function Feed() {
                 {
                     title: "Hello World 2!",
                     id: "second-of-its-kind",
-                    content: "egijwrgiowrjg iowjg oijwrgoijweiog jweig weoig jjwe gjweig owejg iowej gioweg jweiog jweoigj weiogj weg wrgyuiolkijuhgf engjoergn erogneroirngoie goinrogneroignero gnegoirnoiroiren goern eoirng oreing oernig eriog neroig neriog neroi ngreoin  oiengoier  niorege rgojn eroinre wnoer wein weoin noi ewiong "
+                    content: "egijwrgiowrjg iowjg oijwrgoijweiog jweig weoig jjwe gjweig owejg iowej gioweg jweiog jweoigj weiogj weg wrgyuiolkijuhgf engjoergn erogneroirngoie goinrogneroignero gnegoirnoiroiren goern eoirng oreing oernig eriog neroig neriog neroi ngreoin  oiengoier  niorege rgojn eroinre wnoer wein weoin noi ewiong ",
+                    gravity: 0,
+                    postedAt: 1731899211
                 },
                 {
                     title: "Hello World 2!",
-                    id: "secondt34yu6uy-of-its-kind"
+                    id: "secondt34yu6uy-of-its-kind",
+                    gravity: 15,
+                    postedAt: 1731892231
                 },
                 {
                     title: "Hello World 2!",
-                    id: "seco42tet34tnd-of-its-kind"
+                    id: "seco42tet34tnd-of-its-kind",
+                    gravity: 0,
+                    postedAt: 1731893731
                 },
                 {
                     title: "Hello World 2!",
-                    id: "second-of-itjhgfds-kind"
+                    id: "second-of-itjhgfds-kind",
+                    gravity: 0,
+                    postedAt: 1731893291
                 },
             ];
+            
+            
 
-            setPosts(res);
+            setPosts(sortPosts(res));
         }
-        
+
         getPosts();
-        
+
     }, [])
-
-
-    const handleGravityChange = async(id, change) =>{
-        //Post the change to db
-        console.log(id);
-        posts.find((post) => post.id === id).gravity += change;
-    }
 
 
     return (
         <div className={styles.feedWrapper}>
             <Link to="/createPost" className={styles.addPost}>+</Link>
+            <div className={styles.filters}>
+                <h4>Filters</h4>
+                <div>
+                    <label htmlFor="search-filter">Search</label>
+                    <input value={search} id="search-filter" onChange={(e) => setSearch(e.target.value)}></input>
+                </div>
+                <div>
+                    <label>Order by:</label>
+                    <select value={sort} onChange={(e) => setSort(e.target.value)}>
+                        <option value="time">New</option>
+                        <option value="gravity">Gravity</option>
+                        <option value="name">Title</option>
+                    </select>
+                </div>
+
+            </div>
             <div className={styles.feed}>
                 {posts?.map((el, ind) => {
-                    return <Post key={el.id} 
-                                title={el.title} 
-                                id={el.id} 
-                                gravity={el.gravity}
-                                postedAt={el.postedAt}/>;
+                    if (!el.title.toLowerCase().startsWith(search.toLowerCase())) return;
+                    return <Post key={el.id}
+                        title={el.title}
+                        id={el.id}
+                        gravity={el.gravity}
+                        postedAt={el.postedAt} />;
                 })}
+            </div>
+            <div className={styles.featuresVote}>
+                <h4>Vote for new features!</h4>
+                <div>
+
+                </div>
+
             </div>
         </div>
 
