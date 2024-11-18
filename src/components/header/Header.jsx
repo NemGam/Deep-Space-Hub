7f5149a9358a16b2c9564748dd4fee4746aa7b0d
vkrukhmalev <Krukhmalev02@gmail.com>
2024-11-17 19:21:51 -0500

    Made title navigatable to home

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 export default function Header(){
@@ -5,7 +6,7 @@ export default function Header(){
     return(
         <header className={styles.header}>
             <div className={styles.content}> 
-                <h1>DeepSpaceHub 🚀</h1>
+                <Link to="/"><h1>DeepSpaceHub 🚀</h1></Link>
             </div>
             
         </header>
